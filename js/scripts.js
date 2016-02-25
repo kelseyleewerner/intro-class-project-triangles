16e4b06b0f3214e42a38b58b4d47fd406d5950ae
kelseyleewerner <kelsey.lee.werner@gmail.com>
2016-02-24 18:51:46 -0800

    Create function to identify equilateral triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+var triangleCheck = function(sideA, sideB, sideC) {
+  if ((sideA === sideB) && (sideB === sideC)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
 
 
