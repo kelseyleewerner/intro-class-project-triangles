3bf4fe9c7fc4bec632ac6d5cb53e340b00cbcc6b
kelseyleewerner <kelsey.lee.werner@gmail.com>
2016-02-24 20:05:02 -0800

    Connect html with JavaScript business logic functions via JQuery document ready function

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
 var triangleCheck = function(sideA, sideB, sideC) {
   if ((sideA === sideB) && (sideB === sideC)) {
-    return "equilateral";
+    return "an equilateral";
   }
 
   else if ((sideA === sideB) || (sideB === sideC) || (sideA === sideC)) {
-    return "isosceles";
+    return "an isosceles";
   }
 
   else if ((sideA + sideB > sideC) && (sideB + sideC > sideA) && (sideA + sideC > sideB)) {
-    return "scalene";
+    return "a scalene";
   }
 
   else {
-    return "not a triangle";
+    return "not a";
   }
 };
 
 
 
 
-
-
-
-
-
-
-
 $(document).ready(function() {
+  $("form#triangleForm").submit(function(event) {
+    var sideA = parseInt($("input#sideA").val());
+    var sideB = parseInt($("input#sideB").val());
+    var sideC = parseInt($("input#sideC").val());
+    var result = triangleCheck(sideA, sideB, sideC);
+
+    $(".triangleType").text(result);
 
 
-
-
-
-
+    $("#result").show();
+    event.preventDefault();
+  });
 });
